f0ed67ea75d5874d3116c526b7dbefbe0f5f5634
Markoffka90 <annet44_91@mail.ru>
2023-07-30 16:34:44 +0300

    Test for calc

---
+++ tests/first_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_division_success(self):
+        assert self.calc.division(self, 8, 4) == 2
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 7, 2) == 5
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 3, 2) == 5
+
+    # def test_adding_unsuccess(self):
+    #     assert self.calc.adding(self, 1, 1) == 3
+    #
+    # def test_zero_division(self):
+    #     with pytest.raises(ZeroDivisionError):
+    #         self.calc.division(1, 0)
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
